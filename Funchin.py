9fa7e8c139ec6eaaebd18a6cb98b17da6b65c7dd
OB11TO <OB11TO61@yandex.ru>
2020-04-02 17:14:17 +0300

    Разделил все по папкам

---
+++ Funchin.py
@@ -0,0 +1,67 @@
+import pyttsx3
+import os
+import random
+import webbrowser
+import time
+import speech_recognition as sr
+import pandas as pd
+from tkinter import *
+from fuzzywuzzy import fuzz
+from colorama import *
+import sys
+import keyboard
+
+
+
+def hello(): #функция приветствия
+    os.system("echo Приветик. OBIITO. Есть вопросы ? | RHVoice-test -p Anna")
+
+def plans():
+    os.system("echo Моя задача будет состоять в том, чтобы помочь управлять системой Умный дом.\
+                    В настоящее время мы работаем над виртуальной частью программного обеспечения.\
+                    Мы также работаем над оптимизацией всех существующих функций в коде.\
+                    В дальнейшем мы планируем работать над технической частью проекта.\
+                    Она будет состоять из создания элементов умного дома с использованием микроконтроллеров Arduino.\
+                    В конечном итоге виртуальная и техническая части проекта будут объединены.\
+                    Моя конечная цель будет достигнута.| RHVoice-test -p Anna")
+
+def time():
+     import datetime
+     now = datetime.datetime.now()
+     os.system("echo Сейчас время %d часа %d минут| RHVoice-test -p Anna" % (now.hour, now.minute))
+
+
+def clear_analis(): # очистка файла с историей запросов
+    file = open('commands.txt', 'w', encoding = 'UTF-8')
+    file.close()
+    os.system("Файл аналитики был очищен! | RHVoice-test -p Anna")
+
+
+def github():  # открывает гитхаб
+    os.system("echo Открываю твою страницу Гитхаба | RHVoice-test -p Anna")
+    webbrowser.open('https://github.com/OB11TO')
+
+def brows(): # открывает браузер
+    os.system("echo Открываю | RHVoice-test -p Anna")
+    webbrowser.open('https://google.ru'.format(textSearch))
+
+
+def ovk(): # открывает вк
+    os.system("echo Вк открыт  | RHVoice-test -p Anna")
+    webbrowser.open('https://vk.com/feed')
+
+def youtube(): # открывает ютюб
+    os.system("echo Открываю youtube | RHVoice-test -p Anna")
+    webbrowser.open('https://www.youtube.com')
+
+def shut(): # выключает компьютер
+    global quit
+    os.system('sudo shutdown -h now')
+    quit()
+
+def playLILPump(): #включает пампа
+    os.system("echo Включаю Лил пампа | RHVoice-test -p Anna")
+    webbrowser.open('https://www.youtube.com/watch?v=DPxL7dO5XPc')
+
+def musik(): # включает музыку
+    webbrowser.open('https://vk.com/')
--- Thomas.py
+++ Thomas.py
@@ -10,7 +10,8 @@ from fuzzywuzzy import fuzz
 from colorama import *
 import sys
 import keyboard
-import commands
+from Funchin import *
+from cmd import *
 
 
 # Глобальные переменные
@@ -21,12 +22,9 @@ voices = engine.getProperty('voices')
 adress = ''
 j = 0
 task_number = 0
-
+# Имя голосового помошника
 ndel = ['Alexa', 'Алекса', 'Алекс']
 
-commands = ['Приветик','открой гитхаб', 'открой файл', 'down comp', 'выруби компьютер', 'Спасибо', 'покажи файл','покажи список команд',
-'open vk', 'открой браузер', 'open vk', 'открой интернет', 'открой youtube', 'включи музон','вруби музыку','сколько время', 'очисти файл',
-'открой стату', 'покажи cтатистику', 'покажи красивую девушку', 'открой музыку', 'переведи', 'планы', 'на будущее', 'что планируется', 'play lil pump','загугли']
 
 # Описания функций комманд
 def pri_com(): # выводит на экран историю запросов
@@ -60,29 +58,6 @@ def pri_com(): # выводит на экран историю запросов
     }, index = mas4)
     list.index.name = '№'
     print(list)
-
-def plans():
-    global engine
-    os.system("echo Моя задача будет состоять в том, чтобы помочь управлять системой Умный дом.\
-                    В настоящее время мы работаем над виртуальной частью программного обеспечения.\
-                    Мы также работаем над оптимизацией всех существующих функций в коде.\
-                    В дальнейшем мы планируем работать над технической частью проекта.\
-                    Она будет состоять из создания элементов умного дома с использованием микроконтроллеров Arduino.\
-                    В конечном итоге виртуальная и техническая части проекта будут объединены.\
-                    Моя конечная цель будет достигнута.| RHVoice-test -p Anna")
-
-
-def time():
-     import datetime
-     now = datetime.datetime.now()
-     os.system("echo Сейчас время %d часа %d минут| RHVoice-test -p Anna" % (now.hour, now.minute))
-
-
-def clear_analis(): # очистка файла с историей запросов
-    global engine
-    file = open('commands.txt', 'w', encoding = 'UTF-8')
-    file.close()
-    os.system("Файл аналитики был очищен! | RHVoice-test -p Anna")
 
 def add_file(x): # история запросов
     file = open('commands.txt', 'a',encoding = 'UTF-8')
@@ -118,9 +93,6 @@ def clear_task(): #удаляет ключевые слова
         text = text.replace(z,'').strip()
         text = text.replace('  ',' ').strip()
 
-def hello(): #функция приветствия
-    global engine
-    os.system("echo Приветик. OBIITO. Есть вопросы ? | RHVoice-test -p Anna"),
 
 def quit(): # функция выхода из программы
     global engine
@@ -128,35 +100,6 @@ def quit(): # функция выхода из программы
     engine.stop()
     os.system('cls')
     exit(0)
-
-def github():  # открывает гитхаб
-    os.system("echo Открываю твою страницу Гитхаба | RHVoice-test -p Anna")
-    webbrowser.open('https://github.com/OB11TO')
-
-def brows(): # открывает браузер
-    os.system("echo Открываю | RHVoice-test -p Anna")
-    webbrowser.open('https://google.ru'.format(textSearch))
-
-
-def ovk(): # открывает вк
-    os.system("echo Вк открыт  | RHVoice-test -p Anna")
-    webbrowser.open('https://vk.com/feed')
-
-def youtube(): # открывает ютюб
-    os.system("echo Открываю youtube | RHVoice-test -p Anna")
-    webbrowser.open('https://www.youtube.com')
-
-def shut(): # выключает компьютер
-    global quit
-    os.system('sudo shutdown -h now')
-    quit()
-
-def playLILPump(): #включает пампа
-    os.system("echo Включаю Лил пампа | RHVoice-test -p Anna")
-    webbrowser.open('https://www.youtube.com/watch?v=DPxL7dO5XPc')
-
-def musik(): # включает музыку
-    webbrowser.open('https://vk.com/')
 
 def check_translate():
     global text, tr
@@ -187,17 +130,7 @@ def comparison(x): # осуществляет поиск самой подход
         add_file(ans)
     return(str(ans))
 
-cmds = {
-    'Приветик' : hello,                         'выруби компьютер' : shut,                   'down comp' : shut,
-    'Спасибо' : quit,                              'покажи  cтатистику' : pri_com,           'загугли':web_search,
-    'открой браузер' : brows,                  'включи vk' : ovk,                            'открой интернет' : brows,
-    'открой youtube' : youtube,                   'вруби музыку' : musik,                      'open vk' : ovk,
-    'открой  стату' : pri_com,                   'включи музон' : musik,                      'очисти файл' : clear_analis,
-    'покажи файл' : pri_com,                  'открой файл' : pri_com,                  'открой музыку' : musik,
-    'планы' : plans,                           'на будущее' : plans,                      'что планируется' : plans,
-    'открой гитхаб' : github,                     'pump' : playLILPump,                    'сколько время' : time,
-    'переведи' : check_translate,                'play lil pump': playLILPump
-}
+
 
 # распознавание
 def talk():
@@ -252,6 +185,18 @@ def main():
         pass
     except(TypeError):
         pass
+        
+cmds = {
+    'Приветик' : hello,                         'выруби компьютер' : shut,                   'down comp' : shut,
+    'Спасибо' : quit,                              'покажи  cтатистику' : pri_com,           'загугли':web_search,
+    'открой браузер' : brows,                  'включи vk' : ovk,                            'открой интернет' : brows,
+    'открой youtube' : youtube,                   'вруби музыку' : musik,                      'open vk' : ovk,
+    'открой  стату' : pri_com,                   'включи музон' : musik,                      'очисти файл' : clear_analis,
+    'покажи файл' : pri_com,                  'открой файл' : pri_com,                  'открой музыку' : musik,
+    'планы' : plans,                           'на будущее' : plans,                      'что планируется' : plans,
+    'открой гитхаб' : github,                     'pump' : playLILPump,                    'сколько время' : time,
+    'переведи' : check_translate,                'play lil pump': playLILPump
+}
 
 
 # раздел создания интерфейса
